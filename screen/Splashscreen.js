7af787194ad9d15d63cf3213412e21f6bae6cdb7
rajchatterz <iamraj.space@gmail.com>
2023-12-13 16:59:32 +0530

    start working on nres app

---
+++ screen/Splashscreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Splashscreen = () => {
+  return (
+    <View>
+      <Text>Splashscreen</Text>
+    </View>
+  )
+}
+
+export default Splashscreen
