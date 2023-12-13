841e7c574bf22f53cb033ded98e885393b6627cf
rajchatterz <iamraj.space@gmail.com>
2023-12-13 20:56:55 +0530

    done with the fetching the data from api

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { View, Text, FlatList } from 'react-native'
+import React, { useState } from 'react'
+import axios from 'axios'
+import SearchInput from './component/SearchInput'
 const App = () => {
+  const [news, setNews] = useState([])
+  const [searchQuery, setSearchQuery] = useState('')
+  
+  
+  const fetchNews = async () => {
+    try {
+      const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2023-11-13&sortBy=publishedAt&apiKey=e6624f0c3cf54c51a4285cd8efec8524`)
+      setNews(response.data.articles)
+    } catch (error) {
+      console.log("error found while fetching the data", error)
+      
+    }
+  }
+  const handleSearch = () => {
+    fetchNews()
+  } 
+  const handleSearchChange = (text) => { 
+    setSearchQuery(text)
+  }
+  const renderItem = ({ item }) => (
+    <View>
+      <Text>{item.title}</Text>
+    </View>
+  )
   return (
     <View>
-      <Text>App</Text>
+      <SearchInput searchQuery={searchQuery} onSearchChange={handleSearchChange} onSearchPress={handleSearch} />
+      <FlatList
+        data={news}
+        renderItem={renderItem}
+        keyExtractor={(item,index)=>index.toString()}
+      />
     </View>
   )
 }
--- component/SearchInput.jsx
+++ component/SearchInput.jsx
@@ -1,24 +1,59 @@
 import React from 'react';
-import {View, TextInput, Button} from 'react-native';
+import {
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+} from 'react-native';
 
 const SearchInput = ({searchQuery, onSearchChange, onSearchPress}) => {
   return (
-    <View style={{flexDirection: 'row', marginBottom: 10}}>
+    <SafeAreaView style={styles.container}>
       <TextInput
-        style={{
-          flex: 1,
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1,
-          paddingHorizontal: 10,
-        }}
-        placeholder="Enter search query"
+        style={styles.textInputContainer}
+        placeholder="Search News Here"
         onChangeText={onSearchChange}
         value={searchQuery}
       />
-      <Button title="Search" onPress={onSearchPress} />
-    </View>
+      <TouchableOpacity style={styles.buttonContainer} onPress={onSearchPress}>
+        <Text style={styles.buttonText}>Search</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    marginTop: 20,
+    alignItems: 'center',
+    width: '100%',
+    gap: 20,
+  },
+  buttonContainer: {
+    backgroundColor: '#fb9e1d',
+    justifyContent: 'center',
+    height: 50,
+    width: '20%',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  textInputContainer: {
+    flex: 1,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    height: 50,
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontWeight: '900',
+    color: 'white',
+  },
+});
 export default SearchInput;
